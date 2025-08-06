519249294f92cfef960fc65854a661a7d6c48e5e
Nuno Teixeira <nunoteixeiradev@gmail.com>
2025-08-06 15:16:06 +0100

    Add settings page

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,18 +1,47 @@
+import { createSettingsStyles } from '@/assets/styles/settings.styles';
+import DangerZone from '@/components/DangerZone';
+import Preferences from '@/components/Preferences';
+import ProgressStats from '@/components/ProgressStats';
 import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 const SettingsScreen = () => {
-    const { toggleDarkMode, colors } = useTheme();
+    const { colors } = useTheme();
+    const settingsStyles = createSettingsStyles(colors);
 
     return (
-        <SafeAreaView>
-            <Text>SettingsScreen</Text>
+        <LinearGradient
+            colors={colors.gradients.background}
+            style={settingsStyles.container}
+        >
+            <SafeAreaView style={settingsStyles.safeArea}>
+                <View style={settingsStyles.header}>
+                    <View style={settingsStyles.titleContainer}>
+                        <LinearGradient
+                            colors={colors.gradients.primary}
+                            style={settingsStyles.iconContainer}
+                        >
+                            <Ionicons name='settings-outline' size={28} color={"#fff"} />
+                        </LinearGradient>
+                        <Text style={settingsStyles.title}>Settings</Text>
+                    </View>
+                </View>
 
-            <TouchableOpacity onPress={toggleDarkMode}>
-                <Text>Toggle mode</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
+                <ScrollView
+                    style={settingsStyles.scrollView}
+                    contentContainerStyle={settingsStyles.content}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <ProgressStats />
+                    <Preferences />
+                    <DangerZone />
+                </ScrollView>
+
+            </SafeAreaView>
+        </LinearGradient>
     )
 }
 
